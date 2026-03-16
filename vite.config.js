2e06593e9c0e3503261d2a6150e3ece920ca5040
Wilsent Philip <wphilip465@gmail.com>
2026-03-16 15:59:00 +0700

    chore: refresh on save

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: [
-                "resources/routes/**",
-                "routes/**",
-                "resources/views/**",
-                "app/Filament/**",
-                "app/Livewire/**",
-            ],
+            refresh: true,
         }),
         tailwindcss(),
     ],
