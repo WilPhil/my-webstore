8323de4278c92aeffc48355e687124f90e556055
Wilsent Philip <wphilip465@gmail.com>
2026-04-01 20:08:22 +0700

    feat: create send order created mail listener from order created event

---
+++ app/Listeners/SendOrderConfirmationEmailListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SalesOrderCreated;
+use App\Mail\SalesOrderCreatedMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendOrderConfirmationEmailListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(SalesOrderCreated $event): void
+    {
+        Mail::queue(
+            new SalesOrderCreatedMail($event->sales_order)
+        );
+    }
+}
