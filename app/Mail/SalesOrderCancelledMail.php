6773d6db9c5a717594d3caf54bd6217fa3ee3025
Wilsent Philip <wphilip465@gmail.com>
2026-04-02 19:29:59 +0700

    add: mailing for sales order state transition and shipping receipt number updated

---
+++ app/Mail/SalesOrderCancelledMail.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use App\Data\SalesOrderData;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SalesOrderCancelledMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public SalesOrderData $sales_order
+    ) {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: "Halo, {$this->sales_order->customer->full_name}. Pesanan dengan nomor #{$this->sales_order->trx_id} anda telah dibatalkan.",
+            to: "{$this->sales_order->customer->email_address}",
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.orders.cancelled',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
