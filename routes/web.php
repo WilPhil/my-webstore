421433b894133fab669e0352b3f348e87abd5e24
Wilsent Philip <wphilip465@gmail.com>
2026-04-10 12:30:05 +0700

    feat: create routing for static page

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::view('/', 'pages.homepage')->name('home');
-
 Route::livewire('/', 'pages::home.index')->name('home');
 Route::livewire('/products', 'pages::product.catalog')->name('product-catalog');
 Route::livewire('/products/{product:slug}', 'pages::product.detail')->name('product-detail');
 Route::livewire('/cart', 'pages::cart.list')->name('cart-list');
 Route::livewire('/checkout', 'pages::checkout.index')->name('checkout');
 Route::livewire('/order-confirmed/{sales_order:trx_id}', 'pages::sales-order.detail')->name('order-confirmed');
+Route::livewire('/page/{page:slug}', 'pages::static-page.index')->name('static-page');
 
 Route::webhooks('moota/callback');
-
-// Route::view('/product', 'pages.product')->name('product');
-// Route::view('/cart', 'pages.cart')->name('cart');
-// Route::view('/checkout', 'pages.checkout')->name('checkout');
-// Route::view('/order-confirmed', 'pages.order-confirmed')->name('order-confirmed');
-Route::view('/page', 'pages.page')->name('page');
